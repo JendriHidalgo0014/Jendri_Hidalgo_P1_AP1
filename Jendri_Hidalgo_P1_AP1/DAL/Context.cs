49d48d225e2836a6a7c24a729924479a765fd2ea
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-17 18:50:41 -0400

    Se agregó el modelo en el contexto.

--- Jendri_Hidalgo_P1_AP1/DAL/Context.cs
+++ Jendri_Hidalgo_P1_AP1/DAL/Context.cs
@@ -1,6 +1,14 @@
-﻿namespace Jendri_Hidalgo_P1_AP1.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using Jendri_Hidalgo_P1_AP1.Models;
+
+namespace Jendri_Hidalgo_P1_AP1.DAL
 {
-	public class Context
+	public class Context : DbContext
 	{
+		public Context(DbContextOptions<Context> options) : base(options)
+		{
+		}
+		public DbSet<Aportes> Aportes { get; set; }
 	}
 }
+
