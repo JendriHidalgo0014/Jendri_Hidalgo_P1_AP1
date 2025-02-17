996f41dca160820b284cefe69b0c2050e10bf552
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-17 18:48:57 -0400

    Se eliminó la clase Modelo y se creó la clase Aportes en Models.

---
+++ Jendri_Hidalgo_P1_AP1/Models/Aportes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jendri_Hidalgo_P1_AP1.Models
+{
+	public class Aportes
+	{
+		[Key]
+
+		public int AporteId { get; set; }
+
+		[Required(ErrorMessage = "Este campo es requerido")]
+		public string Persona { get; set; }
+
+		[Required(ErrorMessage = "Este campo es requerido")]
+		public string Observacion { get; set; }
+
+		[Required(ErrorMessage = "Este campo es requerido")]
+		public decimal Monto { get; set; }
+
+	}
+}
