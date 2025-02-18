8daa08aad5a5455f03c8aab45e8cf00ee6be1a0e
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-17 20:45:52 -0400

    Servicio creado.

--- Jendri_Hidalgo_P1_AP1/Program.cs
+++ Jendri_Hidalgo_P1_AP1/Program.cs
@@ -1,10 +1,23 @@
 using Jendri_Hidalgo_P1_AP1.Components;
+using Jendri_Hidalgo_P1_AP1.DAL;
+using Jendri_Hidalgo_P1_AP1.Services;
+using Jendri_Hidalgo_P1_AP1.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Obtenemos el constructor para usarlo en el contexto.
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+//Agregamos el contexto al builder con el ConStr.
+builder.Services.AddDbContextFactory<Context>(Options => Options.UseSqlServer(ConStr));
+
+//Inyección del service.
+builder.Services.AddScoped<AportesService>();
 
 var app = builder.Build();
 
--- Jendri_Hidalgo_P1_AP1/Services/Servicio.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Jendri_Hidalgo_P1_AP1.Services
-{
-	public class Servicio
-	{
-	}
-}
